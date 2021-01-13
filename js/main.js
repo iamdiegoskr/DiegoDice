fe5e9268248f362cdbc6bc245f90f7fb84c5479c
diego martinez <juandiego337@hotmail.com>
2021-01-12 19:15:43 -0500

    Add rotate effect to play button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+const btnGreen = document.getElementById('green');
+const btnRed = document.getElementById('red');
+const btnPurple = document.getElementById('purple');
+const btnBlue = document.getElementById('blue');
+const btnPlay = document.getElementById('button-play');
+
+
+class Game {
+    constructor() {
+        this.initialize();
+    }
+
+    initialize() {
+        btnPlay.classList.toggle('hide')
+    }
+
+}
+
+
+
+
+
+
+
 function startGame() {
-    alert("skeit")
+    const game = new Game();
 }
